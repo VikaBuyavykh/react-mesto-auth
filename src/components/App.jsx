08fd9ab0c670fa9bc533fb312e213ce4b67d6f97
Vika Buyavykh <vikabuyavykh@gmail.com>
2024-09-13 16:04:16 +0300

    edit readme and title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import { AppContext } from "../contexts/AppContext";
